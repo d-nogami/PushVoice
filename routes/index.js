c64afc5ab75304a775f61a5f1fc25c50591592df
d-nogami <22715630@jptomac21953.corpusers.net>
2014-12-12 17:08:56 +0900

    Fix Push Notification function and add debug api call.

--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,13 @@ router.post('/api/voice', function(req, res) {
     uploadFile(req, res);
 });
 
+router.get('/api/gcm', function(req, res) {
+    var pushNotification = require('../lib/pushNotification');
+    pushNotification.sendMessage('test', 'Ayaka');
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+        '<h1>Send GCM!</h1>'
+    );
+});
 
 module.exports = router;
